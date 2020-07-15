96aefc522509e7d1ccdad0ddde14213f49308fcd
eddogustian <eddogustian78@gmail.com>
2020-07-15 17:47:56 +0700

    revisi master

--- config/app.php
+++ config/app.php
@@ -269,6 +269,7 @@ return [
         Webkul\Velocity\Providers\VelocityServiceProvider::class,
         Webkul\BookingProduct\Providers\BookingProductServiceProvider::class,
         Webkul\SocialLogin\Providers\SocialLoginServiceProvider::class,
+        Webkul\DataFaker\Providers\DataFakerServiceProvider::class,
     ],
 
     /*
---
+++ packages/Webkul/DataFaker/src/Database/Seeders/CustomerAddressTableDataSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Webkul\DataFaker\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class CustomerAddressTableDataSeeder extends Seeder
+{
+    public function run()
+    {
+        $count = 100;
+        DB::table('customers')->delete();
+        // DB::table('customer_addresses')->delete();
+        DB::table('addresses')->delete();
+
+        $data = factory(\Webkul\Customer\Models\CustomerAddress::class, $count)->create();
+    }
+}
